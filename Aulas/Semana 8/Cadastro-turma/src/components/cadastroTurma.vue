4b314a602db51b6944b074848168d6bc179f5f3b
Julia Moura <juliasilvamoura100@gmail.com>
2022-04-19 20:59:23 -0300

    exercício aula

---
+++ Aulas/Semana 8/Cadastro-turma/src/components/cadastroTurma.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1>Porcentagem de vagas preenchidas</h1>
+  <p class="resultado">  <span>{{ resultado }}</span>%</p>
+  <p v-if="resultado == 100"> {{ chamaAlert }}</p>
+  <button class="adiciona" @click="criarCadastro">Cadastra uma pessoa</button>
+  <button class="subrai" @click="cancelaCadastro">Cancela cadastro</button>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            contador: 0
+        }
+    },
+    props:{
+        vagasTurma:{
+            type:Number, 
+            require: true
+        }
+    },
+    methods:{
+        criarCadastro(){
+            Number(this.contador++)
+        },
+        cancelaCadastro(){
+            Number(this.contador--)
+        }
+    },
+    computed:{
+        resultado(){
+            console.log(this.vagasTurma)
+            return Number((this.contador / this.vagasTurma) * 100 )
+        },
+        chamaAlert(){
+            return alert('Turma lotada')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
