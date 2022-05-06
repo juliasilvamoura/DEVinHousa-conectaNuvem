55db9d6199c5afa8f5311491e10390b15a6b49af
Julia Moura <juliasilvamoura100@gmail.com>
2022-05-06 18:57:49 -0300

    Exercicios 7 e 8

---
+++ Semana 9 e 10 - Exercicios/M1S09-ex7/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <vid class="header">
+      <header>
+          <title v-text="titulo"></title>
+      </header>
+  </vid>
+</template>
+
+<script>
+export default {
+    props:{
+        titulo: {
+            type: String
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
